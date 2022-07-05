a3c7da06f9c94df39fec8cb691f78b9dba414382
qqqbbb <qqqinbox@ya.ru>
2022-07-05 15:27:55 +0300

    Homework 1.10

--- src/Homework_1dot9.java
+++ src/Homework_1dot9.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Homework_1dot9
 {
     public static void main(String[] args)
@@ -7,6 +9,7 @@ public class Homework_1dot9
         Author author2 = new Author("Author2FirstName", "Author2LastName");
         Book book2 = new Book("Book2Name", author1, 2020 );
         book2.setPublishYear(2022);
+        System.out.println( book2);
     }
 }
 class Book
@@ -20,6 +23,32 @@ class Book
         this.name = name;
         this.author = author;
         this.publishYear = publishYear;
+    }
+
+    @Override
+    public String toString()
+    {
+        return  '\'' + name + '\'' +
+                " was written by " + author +
+                " and was published in " + publishYear +
+                '.';
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Book book = (Book) o;
+        return name.equals(book.name) && author.equals(book.author);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(name, author);
     }
 
     public String getName()
@@ -53,6 +82,30 @@ class Author
     {
         this.firstName = firstName;
         this.lastName = lastName;
+    }
+
+    @Override
+    public String toString()
+    {
+        return  firstName +
+                " " + lastName ;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Author author = (Author) o;
+        return firstName.equals(author.firstName) && lastName.equals(author.lastName);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(firstName, lastName);
     }
 
     public String getFirstName()
